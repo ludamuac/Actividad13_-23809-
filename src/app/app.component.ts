3d33efd734455998d72acdedcc4227aec2ef972b
Luis Muñoz <52872249+doggodrill@users.noreply.github.com>
2020-02-23 21:30:11 -0800

    Actualizado 9:30pm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {textareaAutoResize} from 'materialize-css';
+
+declare let $: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Notas';
+  notas = [];
+
+  ngOnInit(): void{
+    $('.collapsible').collapsible();
+  }
+
+  addNote(input1: HTMLInputElement, input2: HTMLInputElement){
+    const titulo = input1.value;
+    const texto = input2.value;
+
+    if (titulo === '') {
+      alert('Ingresa titulo');
+    } else if (texto === '') {
+      alert('Ingresa texto');
+    } else {
+      const nota = {
+        title: titulo,
+        body: texto
+      };
+      this.notas.unshift(nota);
+      $('#titulo').val('')
+      // input2.value = '';
+      $('#textarea1').val('');
+      textareaAutoResize($('#textarea1'));
+    }
+  }
 }
